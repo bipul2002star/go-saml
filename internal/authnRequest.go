e1c3184c0251cb248b1c2052aec001f2df016a7b
Bipul Gautam <56266558+bipul2002star@users.noreply.github.com>
2020-10-01 02:04:20 +0530

    Update authnRequest.go

--- internal/authnRequest.go
+++ internal/authnRequest.go
@@ -9,13 +9,13 @@ var MaxIssueDelay = time.Minute * 10
 
 func (a *AuthnRequest) Validate() error {
 	if a.ID == "" {
-		return fmt.Errorf("request not contain the id")
+		return fmt.Errorf("request not contain the id\n")
 	}
 	if a.IssueInstant.Add(MaxIssueDelay).Before(time.Now()) {
-		return fmt.Errorf("request expired at %s", a.IssueInstant.Add(MaxIssueDelay))
+		return fmt.Errorf("request expired at %s\n", a.IssueInstant.Add(MaxIssueDelay))
 	}
 	if a.Version != "2.0" {
-		return fmt.Errorf("expected SAML request version 2.0 got %v", a.Version)
+		return fmt.Errorf("expected SAML request version 2.0 got %v\n", a.Version)
 	}
 	return nil
 }
